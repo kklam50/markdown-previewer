d110f7aed2db35246516f4ddbb96bde97cd6d726
kylelam <kyleklam2009@gmail.com>
2024-02-04 14:38:36 -0800

    Added initial functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Editor from "./components/Editor";
 import Previewer from "./components/Previewer";
+import { useState } from 'react';
+import { marked } from 'marked';
 
 function App() {
+  const startingMarkdown = "### Hello! \nWelcome to the markdown previewer! \n\n**bolded text**";
+  const [textInput, setTextInput] = useState(startingMarkdown);
+
   return (
     <div className="App">
-      <Editor/>
-      <Previewer/>
+      <Editor textInput={textInput} setTextInput={setTextInput}/>
+      <Previewer textInput={marked.parse(textInput)}/>
     </div>
   );
 }
